0a8fec5f107e802dd209fbad5fa86fa713914d28
rebertwalogjr <rebertwalogjr@gmail.com>
2025-12-01 18:43:59 +0800

    add right-side panel layout

--- src/app/qas/[seriesno]/right-panel.tsx
+++ src/app/qas/[seriesno]/right-panel.tsx
@@ -1,26 +1,74 @@
 import StatusBadge from "@/components/status-badge";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Ellipsis, EllipsisVertical } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { DatePicker } from "@/components/datepicker";
+import { Separator } from "@/components/ui/separator";
 
 
 export default function RightPanel() {
   return (
-    <div className="bg-muted h-full py-4 px-4 border-l overflow-auto">
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center gap-2">
-            Series - #
-            <Label className="text-xl text-primary">1</Label>
-            <StatusBadge status="closed" />
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sapiente ut ea commodi cum nam consequatur, dignissimos earum omnis quas, placeat odio necessitatibus rem ipsum voluptas odit quasi dolorum? Earum?</CardDescription>
-        </CardContent>
-      </Card>
+    <div className="bg-muted py-4 px-4 border-l h-full">
+
+      <div className="flex flex-col h-full bg-background rounded-md border shadow-2xl">
+
+        <div className="flex items-center gap-3 p-4 berder-b border-b">
+          <Label className="text-lg">Series - #10</Label>
+          <StatusBadge status="closed" />
+        </div>
+
+        <div className="flex flex-col gap-6 px-4 py-4 h-full overflow-auto">
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Corrective Action</Label>
+            <Textarea id="comment" placeholder="Type here..." className="resize-none" />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Commitment Date</Label>
+            <DatePicker />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Preventive Action</Label>
+            <Textarea id="comment" placeholder="Type here..." className="resize-none" />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Commitment Date</Label>
+            <DatePicker />
+          </div>
+
+          <Separator />
+          
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Hold From</Label>
+            <DatePicker />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Hold Until</Label>
+            <DatePicker />
+          </div>
+
+          <Separator />
+
+          <div className="flex items-center gap-3">
+            <Checkbox />
+            <Label>Verify</Label>
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="comment">Comments / Remarks</Label>
+            <Textarea id="comment" placeholder="Type here..." className="resize-none" />
+          </div>
+        </div>
+
+        <div className="w-full border-t px-4 py-4">
+          <Button size="lg" className="w-full">Submit</Button>
+        </div>
+
+      </div>
+
     </div>
+
   )
 }
